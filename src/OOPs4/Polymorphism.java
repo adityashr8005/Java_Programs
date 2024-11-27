8841abe3e05ae71769ee578b451e3b397f112283
Aaditya <shr.aadi000@gmail.com>
2024-11-27 10:40:27 +0530

    Overriding cleared

--- src/OOPs4/Polymorphism.java
+++ src/OOPs4/Polymorphism.java
@@ -1,7 +1,24 @@
 package OOPs4;
 
-public class Polymorphism {
+//Overriding...
+
+class abc{
+    void fun(){
+        System.out.println("Hello");
+    }
+}
+public class Polymorphism extends abc{
+
+        void fun(){
+            super.fun();
+            System.out.println("Hi");
+        }
     public static void main(String[] args) {
 
+        Polymorphism obj = new Polymorphism();
+        obj.fun();
+        //It will access child class fun because it overrides the parent class.
+        //So,to access parent class function we use super key word.
+        //Super keyword is used to access the super class.
     }
 }
