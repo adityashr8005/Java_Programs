3e647358de7e057b469b1091a8899a5271188f8f
Aaditya <shr.aadi000@gmail.com>
2024-11-27 15:40:22 +0530

    dual loop done

---
+++ src/DualLoop/Loop0.java
@@ -0,0 +1,14 @@
+package DualLoop;
+
+//3 to 7 tables.
+public class Loop0 {
+    public static void main(String[] args) {
+
+        for (int num1=3 ; num1<=7 ; num1++){
+            for(int i=1 ; i<=10 ; i++){
+                System.out.print(num1*i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
