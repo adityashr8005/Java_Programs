023f1e77642142bf3b27683582e4e9e5814a2f6c
Aaditya <shr.aadi000@gmail.com>
2024-12-03 10:49:25 +0530

    learned

--- src/OOPs3/Inheritance.java
+++ src/OOPs3/Inheritance.java
@@ -6,14 +6,15 @@ class xyz{
     }
 }
 
-class abc { // abc inherit properties of xyz
+class abc extends xyz{ // abc inherit properties of xyz
     void fun(){
         System.out.println("abc called.");
     }
 }
-public class Inheritance extends abc{ //abc is parent class and test4 is child class.
+public class Inheritance extends abc{ //abc is parent class and Inheritance is child class.
     public static void main(String[] args) {
         Inheritance obj = new Inheritance();
         obj.fun();
+        obj.sun();
     }
 }
